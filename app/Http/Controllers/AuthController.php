8053032e1637c8af4198960877c41476e9de42ca
Lawrence Caulker <locaulker@gmail.com>
2019-02-09 21:14:36 -0500

    user login completed

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\User;
 // use Illuminate\Http\Request;
 use App\Http\Requests\UserRegisterRequest;
+use App\Http\Requests\UserLoginRequest;
 use App\Http\Resources\User as UserResource;
 
 class AuthController extends Controller
@@ -22,9 +23,24 @@ class AuthController extends Controller
 		}
 		return (new UserResource($request->user()))->additional([
 			'meta' => [
-				'token' => $token,
-
+				'token' => $token
 			]
 		]);
 	}
-}
+
+
+	public function login(UserLoginRequest $request) {
+		if ( !$token = auth()->attempt($request->only(['email', 'password'])) ) {
+			return response()->json([
+				'errors' => [
+					'email' => ['Sorry, information provided is incorrect']
+				]
+				], 422);
+		}
+			return (new UserResource($request->user()))->additional([
+				'meta' => [
+					'token' => $token
+				]
+			]);
+		}
+	}
